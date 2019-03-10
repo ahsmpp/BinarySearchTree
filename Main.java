f997f9b965667f9562ceb07ce0f18795e231778a
Anish <44174596+ahsmpp@users.noreply.github.com>
2019-03-10 15:54:01 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) 
+    {/*
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(52);
+        tree.root.left = new Node(28);
+        tree.root.left.left = new Node(14);
+        tree.root.left.left.left = new Node(7);
+        tree.root.left.left.right = new Node(21);
+        tree.root.left.right = new Node(36);
+        tree.root.left.right.left = new Node(30);
+        tree.root.left.right.right = new Node(42);
+        tree.root.right = new Node(76);
+        tree.root.right.left = new Node(62);
+        tree.root.right.right = new Node(90);
+        tree.root.right.right.left = new Node(80);
+        tree.root.right.right.right = new Node(100);
+        
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        
+        int low = firstNumber > secondNumber ? secondNumber : firstNumber;
+        int high = firstNumber > secondNumber ? firstNumber : secondNumber;
+
+        System.out.println(tree.giftAmount(tree.root, low, high));
+        BinaryTree tree = new BinaryTree(); 
+        tree.root = new Node(20); 
+        tree.root.left = new Node(8); 
+        tree.root.right = new Node(22); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(12); 
+        tree.root.left.right.left = new Node(10); 
+        tree.root.left.right.right = new Node(14);
+        System.out.println(tree.func(tree.root));
+  
+        int n1 = 10, n2 = 14; 
+        Node t = tree.lca(tree.root, n1, n2); 
+        System.out.println(t.value); 
+   
+        n1 = 14; 
+        n2 = 8; 
+        t = tree.lca(tree.root, n1, n2); 
+        System.out.println(t.value); 
+   
+        n1 = 10; 
+        n2 = 22; 
+        t = tree.lca(tree.root, n1, n2); 
+        System.out.println(t.value);
+        tree.printAncestors(tree.root, 14);
+        BinaryTree tree = new BinaryTree();
+        
+
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.right = new Node(4); 
+        tree.root.right.left = new Node(5); 
+        tree.root.right.left.right = new Node(6); 
+        /ree.printSingles(tree.root);
+        tree.sumchildren(tree.root);
+        int levels[] = {2, 3, 3, 3, 3, 2, 1};
+        int values[] = {70, 23, 60, 78, 18, 20, 50};
+        tree.root = tree.arrayBST(array, 0, array.length - 1);
+        tree.root = tree.construct(tree.root, levels, values);
+        tree.printpreorder(tree.root);
+        System.out.println();
+        tree.printinorder(tree.root);
+        System.out.println();
+        tree.printpostorder(tree.root);
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int levels[] = new int[length];
+        for(int i = 0; i < length; i++) {
+            levels[i] = sc.nextInt();
+        }
+        int values[] = new int[length];
+        for(int i = 0; i < length; i++) {
+            values[i] = sc.nextInt();
+        }
+
+        Node root = null;
+        root = tree.insertRec(root, levels, values);
+        tree.printpostorder(root);
+        int array[] = new int[64];
+        for(int i = 0; i < 64; i++) {
+            array[i] = i + 1;
+        }
+        tree.root = tree.arrayBST(array, 0, array.length - 1);
+        System.out.println(tree.func(tree.root));*/
+    }
+}
